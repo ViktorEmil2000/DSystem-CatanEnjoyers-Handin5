0f0835dd645e945f06bb982cb06c3b3eb899f956
Sweatyfish <106493731+Sweatyfish@users.noreply.github.com>
2024-11-21 16:39:03 +0100

    hey

--- auctionBidder/auctionBidService.go
+++ auctionBidder/auctionBidService.go
@@ -2,10 +2,52 @@ package auctionBidder
 
 import "context"
 
+var (
+	auctionLive bool
+	highestbid  bid
+	bidders     []bidder
+)
+
+type bidder struct {
+	id   int64
+	bids []bid
+}
+
+type bid struct {
+	bidammount int64
+	timestamp  int64
+}
+
 type AuctionBidderService struct {
 }
 
 func (ABS *AuctionBidderService) Bid(context.Context, *FromBidder) (*FromAuction, error) {
+	if auctionLive == false {
+		auctionLive = true
+	}
+	check := false
+	for _, element := range bidders {
+		if (element.id == FromBidder{}.ID) {
+			check = true
+			element.bids = append(element.bids, bid{FromBidder{}.Amount, FromBidder{}.Timestamp})
+		}
+	}
+	if check == false {
+		temp := []bid{}
+		temp = append(temp, bid{FromBidder{}.Amount, FromBidder{}.Timestamp})
+		bidders = append(bidders, bidder{FromBidder{}.ID, temp})
+	}
+
+	gotHighestBid := false
+	if (highestbid.bidammount < FromBidder{}.Amount || (highestbid.bidammount == FromBidder{}.Amount && highestbid.timestamp > FromBidder{}.Timestamp)) {
+		highestbid = bid{FromBidder{}.ID, FromBidder{}.Timestamp}
+		gotHighestBid = true
+	}
+	if gotHighestBid {
+		return &FromAuction{Acknowledgment: gotHighestBid, Comment: "You got the higest bid"}, nil
+	} else {
+		return &FromAuction{Acknowledgment: gotHighestBid, Comment: "bidNotHighEnough"}, nil
+	}
 
 }
 
