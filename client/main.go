22f5efde1b45f3eb2952a1a34a032dc3024a382b
alhhITU <alhh@itu.dk>
2024-11-22 15:25:43 +0100

    beginning of bid method done (checks result to accordingly act)

--- client/main.go
+++ client/main.go
@@ -23,7 +23,6 @@ type result struct {
 }
 
 func main() {
-	fmt.Println("Enter port:")
 	var MoneyAmount = int64(100000)
 	var userId = int64(rand.Intn(10000000))
 	var highestBid = int64(0)
@@ -31,31 +30,60 @@ func main() {
 }
 
 func initializeBidder(userId int64, highestBid int64, MoneyAmount int64) {
-
 	conn, err := grpc.Dial("localhost:50051", grpc.WithInsecure())
 	if err != nil {
 		log.Fatalf("Failed to dial AuctionServer %s:", err)
 	}
 	Client := auctionBidder.NewCommunicationClient(conn)
 
-	result, err := Client.Result(context.Background(), &auctionBidder.Empty{})
+	result, err := startBidding(Client, userId, highestBid, MoneyAmount)
 	if err != nil {
 		log.Fatalf("Could not get result %s:", err)
 	}
-	highestBid = res
 
-	Client.Bid(context.Background(), &auctionBidder.FromBidder{
-		Amount:    sendBid(highestBid),
-		ID:        userId,
-		Timestamp: time.Now().Unix(),
-	})
-	auctionBidder.FromAuction()
-
+	if result.ID == userId {
+		fmt.Printf("I have won! my ID was: %s", userId)
+	} else {
+		fmt.Printf("I didn't win! my ID was: %s", userId)
+	}
 }
 
-func sendBid(highestBid int64) int64 {
+func startBidding(Client auctionBidder.CommunicationClient, userId int64, highestBid int64, MoneyAmount int64) (*auctionBidder.Result, error) {
+	for{
+		time.Sleep(100)
+		result, err := Client.Result(context.Background(), &auctionBidder.Empty{})
+		if err != nil {
+			log.Fatalf("Could not get receive %s:", err)
+		}
+		if(result.AuctionOver){
+			return result, err
+		}
+		
+		if result.AuctionActive {
+			if userId != result.ID {
+				highestBid = result.Amount
+				log.Printf("Received message from server: %s", result.Comment)
+
+				if highestBid > MoneyAmount {
+					log.Printf("%s: with %s I can't afford to bid higher than the current bid: %s, so I'm outta here", userId, MoneyAmount, highestBid)
+					return result, err
+				} else {
+					sendBid(Client, userId, highestBid)
+				}
+
+			} else {
+				fmt.Print("I am currently the highest bidder, and do not need to bid")
+				time.Sleep(100)
+			}
+		}
+	}
+}
+
+func sendBid(Client auctionBidder.CommunicationClient, userId int64, highestBid int64) int64 {
 	var bidAmount = highestBid + int64(rand.Intn(900)+100)
-	return bidAmount
+	Client.Bid()
+
+	
 
 }
 
